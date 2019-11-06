46a4c20214a79e47be748f6fa9b63fc3c3fef64a
boazpinto <pintob@gmail.com>
2019-11-06 15:34:12 +0200

    select month ready to use

--- src/components/hoursApprove/selectMonth.js
+++ src/components/hoursApprove/selectMonth.js
@@ -6,15 +6,54 @@ import '../../pages/hours/hoursApprove.css'
 
 
 export default class SelectMonth extends React.Component {
-    constructor(props){
-        super(props);   
+    constructor(props) {
+        super(props);
     }
     render() {
+        const { month,monthBack,monthForward } = this.props;
+        let monthText = "";
+        switch (month) {
+            case 1: monthText = "ינואר"
+                // code block
+                break;
+            case 2: monthText = "פברואר"
+                // code block
+                break;
+            case 3: monthText = "מרץ"
+                // code block
+                break;
+            case 4: monthText = "אפריל"
+                // code block
+                break;
+            case 5: monthText = "מאי"
+                // code block
+                break;
+            case 6: monthText = "יוני"
+                // code block
+                break;
+            case 7: monthText = "יולי"
+                // code block
+                break;
+            case 8: monthText = "אוגוסט"
+                // code block
+                break;
+            case 9: monthText = "ספטמבר"
+                // code block
+                break;
+            case 10: monthText = "אוקטובר"
+                // code block
+                break;
+            case 11: monthText = "נובמבר"
+                // code block
+                break;
+            case 12: monthText = "דצמבר"
+            // code block
+        }
         return (
             <div className="chooseMonth">
-                <img className="arrowleftright" src="/images/ArrowRight/drawable-xxhdpi/arrow_down.png"/>
-                <span className="monthTag">   {this.props.month}  </span>
-                 <img  className="arrowleftright" src="/images/ArrowLeft/drawable-xxhdpi/arrow_down.png"/>
+                <img className="arrowleftright" src="/images/ArrowRight/drawable-xxhdpi/arrow_down.png" onClick={()=>{monthBack()}}/>
+                <span className="monthTag">   {monthText}  </span>
+                <img className="arrowleftright" src="/images/ArrowLeft/drawable-xxhdpi/arrow_down.png"  onClick={monthForward}/>
             </div>
         );
     }
--- src/pages/hours/HoursApprovePage.js
+++ src/pages/hours/HoursApprovePage.js
@@ -6,16 +6,44 @@ import { Redirect } from 'react-router-dom'
 import SelectMonth from '../../components/hoursApprove/selectMonth'
 
 class HoursApprovePage extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+                year:new Date().getFullYear(),
+                month:new Date().getMonth()+1
+            }
+        
+    }
+    monthBack=()=>{
+        let {month,year}=this.state;
+        const curYear=new Date().getFullYear();
+        const curMonth=new Date().getMonth()+1;
+        if (month===1) 
+                    {month=12;year--;}
+        else 
+                    {month--}
+        if ((year===curYear&& month<=curMonth) || (year<curYear && month>curMonth)) this.setState({month,year})
+    }
+    monthForward=()=>{
+        let {month,year}=this.state;
+        const curYear=new Date().getFullYear();
+        const curMonth=new Date().getMonth()+1;
+        if (month===12) 
+                    {month=1;year++;}
+        else 
+                    {month++}
+        if ((year===curYear&& month<=curMonth) || (year<curYear && month>curMonth)) this.setState({month,year})   
+    }
     render() {
-
+  
         if (!this.props.activeUser) {
             return <Redirect to='/' />
         }
-
+        const {year,month}=this.state;
         return (
             <div>
                 <PortalNavbar/>
-                <SelectMonth month="פברואר"/>
+                <SelectMonth month={parseInt(this.state.month)} monthBack={this.monthBack} monthForward={this.monthForward}/>
                 <div>
                     חיפוש עובדים
                 </div>

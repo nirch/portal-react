a284cb2580e83126ea220f8053e8dadd436878bf
ilyakoptev <ilya.koptev@gmail.com>
2019-11-07 10:18:35 +0200

    add calculate hours

--- src/pages/hours/HoursReportPage.js
+++ src/pages/hours/HoursReportPage.js
@@ -35,18 +35,35 @@ class HoursReportPage extends Component {
         })
         
     }
+    
+     diff(start, end) {
+        start = start.split(":");
+        end = end.split(":");
+        var startDate = new Date(0, 0, 0, start[0], start[1], 0);
+        var endDate = new Date(0, 0, 0, end[0], end[1], 0);
+        var diff = endDate.getTime() - startDate.getTime();
+        var hours = Math.floor(diff / 1000 / 60 / 60);
+        diff -= hours * 1000 * 60 * 60;
+        var minutes = Math.floor(diff / 1000 / 60);
+    
+        // If using time pickers with 24 hours format, add the below line get exact hours
+        if (hours < 0)
+           hours = hours + 24;
+        console.log((hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes)
+        return (hours <= 9 ? "0" : "") + hours + ":" + (minutes <= 9 ? "0" : "") + minutes;
+    }
     //https://pil1.appleseeds.org.il/dcnir/server/datagate.php?type=GetNetaCandidatesViewers
     //https://pil1.appleseeds.org.il/dcnir/server/datagate.php?type=GetReports
     //https://pil1.appleseeds.org.il/dcnir/server/datagate.php?type=GetMyReportingPerimeter
     render() {
 
-         const { courses, GetReports } = this.state;
+         const { GetReports } = this.state;
 
         if (!this.props.activeUser) {
             return <Redirect to='/' />
         }
         console.log(GetReports)
-
+        
         let rows =  GetReports.map((item) => {  // generate table with customers
                 let bgStyle; 
                 switch (item.checkdate) {
@@ -57,6 +74,7 @@ class HoursReportPage extends Component {
                          bgStyle = " bg-success "   
                  }
                  let style = " report-status mt-2 py-3 " + bgStyle
+                 let hoursDiff = this.diff(item.starthour,item.finishhour)
                  return  <Row className={style}>
                       <Col className="px-1 text-center">
                        {item.date}
@@ -68,7 +86,7 @@ class HoursReportPage extends Component {
                       {item.courseid}
                       </Col>
                       <Col className="px-1 text-center">
-                      {item.hours}
+                      {hoursDiff}
                       </Col>
                    </Row>
          }
@@ -76,6 +94,7 @@ class HoursReportPage extends Component {
      
         return (
             <Container className="report-font-size" >
+             <PortalNavbar/>
               <Row className="justify-content-md-center report-font-bold">
               <Col xs  className="px-1 text-center" >
                     <span>תאריך</span>

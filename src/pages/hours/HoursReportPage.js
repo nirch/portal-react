1807e24da43affa9293cafe901130fd1eff62483
ilyakoptev <ilya.koptev@gmail.com>
2019-11-07 10:04:37 +0200

    get data from server

--- src/pages/hours/HoursReportPage.js
+++ src/pages/hours/HoursReportPage.js
@@ -4,96 +4,89 @@ import PortalNavbar from '../../components/navbar/PortalNavbar';
 import { connect } from "react-redux";
 import { Redirect } from 'react-router-dom'
 import { Container, Button , Table, Modal, Row, Col} from 'react-bootstrap';
+import server from '../../shared/server'
 
 class HoursReportPage extends Component {
   
     constructor(props) {
         super(props);
         this.state = {
-                courses :[{
-                            actionid: "1",
-                            approval: "0",
-                            automatic: "0",
-                            carkm: null,
-                            checkdate: null,
-                            comment: "",
-                            cost: null,
-                            courseid: "398",
-                            date: "05/11/2019",
-                            finishhour: "14:45",
-                            missingreportsubject: "0",
-                            projectid: "6",
-                            reportcopyid: "80925",
-                            reportid: "81687",
-                            starthour: "13:00",
-                            userid: "933",
-                        project: "UP PROGRESS",
-                        actionName: "שם פעילות כללית",
-                        hours: "9"
-                     },
-                     {
-                        actionid: "1",
-                        approval: "0",
-                        automatic: "0",
-                        carkm: null,
-                        checkdate: null,
-                        comment: "",
-                        cost: null,
-                        courseid: "398",
-                        date: "05/11/2019",
-                        finishhour: "14:45",
-                        missingreportsubject: "0",
-                        projectid: "6",
-                        reportcopyid: "80925",
-                        reportid: "81687",
-                        starthour: "13:00",
-                        userid: "933",
-                    project: "UP PROGRESS",
-                    actionName: "שם פעילות כללית",
-                    hours: "9"
-                 }
-                    ]}
+            GetReports: [],
+            month: 11,
+            year: 2019,
+        }
         }
     
-   
+    componentDidMount(){
+        var data = {
+            month : this.state.month,
+            year : this.state.year
+        };
+        server(data, "GetReports").then(res => {
+            console.log(res);
+            if (res.data.error) {
+                alert("error in login");
+            } else {
+                data = res.data;
+                this.setState({GetReports:data})
+            }
+        }, err => {
+            console.error(err);
+        })
+        
+    }
+    //https://pil1.appleseeds.org.il/dcnir/server/datagate.php?type=GetNetaCandidatesViewers
+    //https://pil1.appleseeds.org.il/dcnir/server/datagate.php?type=GetReports
+    //https://pil1.appleseeds.org.il/dcnir/server/datagate.php?type=GetMyReportingPerimeter
     render() {
 
-         const { courses } = this.state;
+         const { courses, GetReports } = this.state;
 
         if (!this.props.activeUser) {
             return <Redirect to='/' />
         }
+        console.log(GetReports)
 
-        let rows =  courses.map(item =>   // generate table with customers
-                   <Row my="3" className="report-status">
-                      <Col>
+        let rows =  GetReports.map((item) => {  // generate table with customers
+                let bgStyle; 
+                switch (item.checkdate) {
+                     case null: 
+                        bgStyle =  " bg-warning "  
+                        break;
+                     default:  
+                         bgStyle = " bg-success "   
+                 }
+                 let style = " report-status mt-2 py-3 " + bgStyle
+                 return  <Row className={style}>
+                      <Col className="px-1 text-center">
                        {item.date}
+                      </Col >
+                      <Col className="px-1 text-center">
+                      {item.projectid}
                       </Col>
-                      <Col>
-                      {item.project}
+                      <Col className="px-1 text-center">
+                      {item.courseid}
                       </Col>
-                      <Col>
-                      {item.actionName}
-                      </Col>
-                      <Col>
+                      <Col className="px-1 text-center">
                       {item.hours}
                       </Col>
                    </Row>
+         }
         )
      
         return (
             <Container className="report-font-size" >
-              <Row className="justify-content-md-center">
-              <Col xs  md="auto" >
+              <Row className="justify-content-md-center report-font-bold">
+              <Col xs  className="px-1 text-center" >
                     <span>תאריך</span>
                   </Col>
-                  <Col xs  md="auto">
+                  <Col xs  className="px-1 text-center">
                     <span>פרויקט</span>
                   </Col>
-                   <Col xs  md="auto">
+                   <Col xs className="px-1 text-center">
                     <span>נושא פעילות</span>
                   </Col>
-                  <Col xs  md="auto">
+                  <Col xs  className="px-1 text-center" >
                     <span>סהייכ שעות</span>
                   </Col>
                   

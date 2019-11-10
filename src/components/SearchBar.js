1f6b4afda691f06c930d6f10a7ab7de7fe9e77dd
Olku-kun <kraoly@gmail.com>
2019-11-10 10:12:03 +0200

    creating css for  search input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,25 @@
 import React from 'react';
 // import { Form, Button, FormControl} from 'react-bootstrap';
 import '../pages/courses/courses.css';
-
+import './SearchBar.css'
 
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {inputvalue: ''}
+        this.state = { inputvalue: '' }
     }
     handleChange = (e) => {
-        this.setState({ inputvalue: e.target.value})
+        this.setState({ inputvalue: e.target.value })
         console.log(this.state.inputvalue)
     }
     render() {
         const searchValue = this.state.inputvalue
         return (
-            <div>
-                <form onSubmit = {() => {this.props.handleSearch(searchValue)}}>
-                    <input type = "text" placeholder = {this.props.searchLabel} value = {this.state.inputvalue} onChange = {this.handleChange}/>
-                    
+            <div className = "search-bar">
+                <form onSubmit={() => { this.props.handleSearch(searchValue) }}>
+                    <input type="text" placeholder={this.props.searchLabel} value={this.state.inputvalue} onChange={this.handleChange} />
                 </form>
-               
+
             </div>
         )
     }

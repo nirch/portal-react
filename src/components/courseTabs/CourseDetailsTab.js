ae81a81d5473a9649afd6e82ff61110b5a5d6df8
Olku-kun <kraoly@gmail.com>
2019-11-17 15:28:31 +0200

    creating courseDetailsTab component

---
+++ src/components/courseTabs/CourseDetailsTab.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import server from '../../shared/server';
+import '../../pages/courses/courses.css';
+
+class CourseDetailsTab extends React.Component {
+constructor(props){
+    super(props);
+    this.state = {}
+}
+
+    render(){
+        return(
+            <div>
+                shalom
+            </div>
+        )
+    }
+}
+
+export default CourseDetailsTab;

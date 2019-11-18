7c2e7bce4184d5662a472f93468a285635bc1f55
Olku-kun <kraoly@gmail.com>
2019-11-18 10:33:40 +0200

    creating CourseSyllabus component

---
+++ src/components/courseTabs/CourseSyllabus.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import server from '../../shared/server';
+import '../../pages/courses/courses.css';
+
+class CourseSyllabus extends React.Component {
+constructor(props){
+    super(props);
+    this.state = {}
+}
+
+    render(){
+        return(
+            <div>
+                CourseSyllabus
+            </div>
+        )
+    }
+}
+
+export default CourseSyllabus;
